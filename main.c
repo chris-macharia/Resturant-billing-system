6cdb2e37ffd83dcdf2bdd510bcb446d14b70ff74
chris-macharia <christophermaina003@gmail.com>
2022-12-07 12:24:55 +0300

    Generate and print out the invoice for the user

--- main.c
+++ main.c
@@ -2,28 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-// function to generate bills
-void generateBillsHeader(char name[50], char date[30]){
-    printf("\n\n");
-        printf("\t ADV. Resturant");
-        printf("---------------------------------");
-        printf("\nDate:%s", date);
-        printf("\nInvoice To: %s", name);
-        printf("\n");
-        printf("---------------------------------");
-        printf("Items\t\t");
-        printf("Qty\t\t");
-        printf("Total\t\t");
-        printf("---------------------------------");
-        printf("\n\n");
-}
-
-void generateBillBody(char items[30], int qty, float price){
-    printf("%s\t\t", items);
-    printf("%.2f\t\t", qty * price);
-    printf("\n");
-}
-
 struct items{
     char item[50];
     float price;
@@ -37,15 +15,37 @@ struct orders{
     struct items itm[50];
 };
 
+// function to generate bills
+void generateBillsHeader(char name[50], char date[30]){
+    printf("\n\n");
+        printf("\t ADV. Resturant\n");
+        printf("-------------------------------------------\n");
+        printf("\nDate:%s", date);
+        printf("\nInvoice To: %s", name);
+        printf("\n");
+        printf("-------------------------------------------\n");
+        printf("Items\t\t");
+        printf("Qty\t\t");
+        printf("Total\t\t");
+        printf("\n-------------------------------------------\n");
+        printf("\n\n");
+}
+
+void generateBillBody(char items[30], int qty, float price){
+    printf("%s\t\t", items);
+        printf("%.2f\t\t", qty * price);
+        printf("\n");
+}
+
 void generateBillFooter(float total){
     printf("\n");
     float dis = 0.1*total;
     float netTotal = total-dis;
     float cgst=0.09*netTotal,grandTotal=netTotal + 2*cgst;
-    printf("---------------------------------");
-    printf("Sub Total \t\t\t%.2f",netTotal);
+    printf("---------------------------------\n");
+    printf("Sub Total \t\t\t%.2f",total);
     printf("\nDiscount @10%s\t\t\t%.2f", "%", dis);
-    printf("\n \t\t\t------------");
+    printf("\n \t\t\t---------------------------");
     printf("\nNet total \t\t\t%.2f", netTotal);
     printf("\nCGST @9%s\t\t\t%.2f", "%", cgst);
     printf("\nSGST @9%s\t\t\t%.2f", "%", cgst);
@@ -58,6 +58,7 @@ void generateBillFooter(float total){
 int main()
 {
     int opt,n;
+    float total;
     struct orders ord; //orders as ord
 
 
@@ -81,6 +82,7 @@ int main()
         strcpy(ord.date, __DATE__); //Sets the date
         printf("\n Please enter the number of items:\t");
         scanf("%d",&n);
+        ord.numOfItems = n;
 
         for(int i=0; i<n; i++){
             fgetc(stdin);
@@ -91,7 +93,14 @@ int main()
             scanf("%d", &ord.itm[i].qty);
             printf("\nPlease enter the unit price: \t");
             scanf("%f", &ord.itm[i].price);
+            total += ord.itm[i].qty * ord.itm[i].price;
         }
+
+        generateBillsHeader(ord.customer,ord.date);
+        for(int i=0;i<ord.numOfItems;i++){
+            generateBillBody(ord.itm[i].item, ord.itm[i].qty, ord.itm[i].price);
+        }
+        generateBillFooter(total);
     }
     printf("\n\n");
 
