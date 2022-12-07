c20e7c16fdd912b78630cda4c32d7a530334f2e0
chris-macharia <christophermaina003@gmail.com>
2022-12-07 11:46:33 +0300

    Switch for case 1 of the user input

--- main.c
+++ main.c
@@ -24,6 +24,19 @@ void generateBillBody(char items[30], int qty, float price){
     printf("\n");
 }
 
+struct items{
+    char item[50];
+    float price;
+    int qty;
+};
+
+struct orders{
+    char customer[50];
+    char date[50];
+    int numOfItems;
+    struct items itm[50];
+};
+
 void generateBillFooter(float total){
     printf("\n");
     float dis = 0.1*total;
@@ -44,13 +57,42 @@ void generateBillFooter(float total){
 
 int main()
 {
+    int opt,n;
+    struct orders ord; //orders as ord
+
+
     printf("\t======= ADV. RESTURSANT =======");
+    printf("\n\nPease select your prefered operation");
     printf("\n\n1.Generate Invoice");
     printf("\n2.Show all Invoices");
     printf("\n3.Search Invoice");
     printf("\n4.Exit");
 
-    printf("\n\nPlease select your prefered operation");
+    printf("\n\n Your choice:\t");
+    scanf("%d",&opt);
+    fgetc(stdin);
+
+    //Switching between the options
+    switch(opt){
+        case 1:
+        printf(" \n Please enter the name of the customer\t");
+        fgets(ord.customer,50, stdin);
+        ord.customer[strlen(ord.customer)-1] = 0;//to remove the \n at the end of the input
+        strcpy(ord.date, __DATE__); //Sets the date
+        printf("\n Please enter the number of items:\t");
+        scanf("%d",&n);
+
+        for(int i=0; i<n; i++){
+            fgetc(stdin);
+            printf("\n\n");
+            printf("Please enter the item %d: ", i+1);
+            fgets(ord.itm[i].item, 20,stdin);
+            printf("\nPlease enter the quantity:\t");
+            scanf("%d", &ord.itm[i].qty);
+            printf("\nPlease enter the unit price: \t");
+            scanf("%f", &ord.itm[i].price);
+        }
+    }
     printf("\n\n");
 
     return 0;
